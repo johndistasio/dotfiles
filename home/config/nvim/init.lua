b3f7119de8a3106c02e51d52e64426edb4b98a28
John Distasio <john@getparker.com>
2022-12-07 22:27:56 -0500

    Add keymaps

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.g.python3_host_prog = vim.env.HOME .. '/.venv/neovim/bin/python'
 
 require 'johndistasio.options'
 require 'johndistasio.plugins'
+require 'johndistasio.keymaps'
 
 vim.cmd('colorscheme everforest')
 
---
+++ home/config/nvim/lua/johndistasio/keymaps.lua
@@ -0,0 +1,7 @@
+local keymap = vim.keymap.set
+
+-- Auto-format and highlight a temporary JSON document and allow close with plain :q
+keymap('n', '<leader>j', ':set ft=json <bar> %!jq<CR> <bar> :setlocal nobuflisted buftype=nofile bufhidden=wipe noswapfile<CR>')
+
+-- Highlight a temporary YAML document and allow close with plain :q
+keymap('n', '<leader>y', ':set ft=yaml <bar> :setlocal nobuflisted buftype=nofile bufhidden=wipe noswapfile<CR>')
