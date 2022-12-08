def4e2bcb3d0d9dee8d6408fd8ffac97ca0cdff3
John Distasio <john@getparker.com>
2022-12-07 22:13:47 -0500

    Add initial lua configuration

---
+++ home/config/nvim/lua/johndistasio/plugins.lua
@@ -0,0 +1,13 @@
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use 'tpope/vim-vinegar'
+
+  -- Colorschemes
+  use 'sainnhe/everforest'
+  use 'icymind/NeoSolarized'
+  use 'joshdick/onedark.vim'
+  use 'junegunn/seoul256.vim'
+  use 'morhetz/gruvbox'
+  use 'mhartington/oceanic-next'
+end)
