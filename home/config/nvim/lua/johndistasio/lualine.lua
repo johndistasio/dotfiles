563bdc29cf34f36f18391486f227fa661b7cffe6
John Distasio <john@getparker.com>
2022-12-28 10:48:48 -0500

    Include full path with tilde in the filename section of the statusbar

--- home/config/nvim/lua/johndistasio/lualine.lua
+++ home/config/nvim/lua/johndistasio/lualine.lua
@@ -3,6 +3,11 @@ return require('lualine').setup {
     icons_enabled = false,
     component_separators = { left = '|', right = '|'},
     section_separators = { left = '', right = ''},
-  }
+  },
+
+  sections = {
+    -- Include full path with tilde in the filename section of the statusbar.
+    lualine_c = { 'filename', {'filename', path = 3} },
+  },
 }
 
