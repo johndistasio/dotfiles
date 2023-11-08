9a4ebbd179c729d07110b1422a580e164453f0c5
John Distasio <john@idiotvilla.xyz>
2023-11-07 20:00:05 -0500

    nvim: update telescope from 0.1.1 to 0.1.4

--- home/config/nvim/lua/johndistasio/packer.lua
+++ home/config/nvim/lua/johndistasio/packer.lua
@@ -10,7 +10,7 @@ return require('packer').startup(function(use)
   use 'https://git.sr.ht/~whynothugo/lsp_lines.nvim'
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { 'nvim-lua/plenary.nvim' }
+    'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = { 'nvim-lua/plenary.nvim' }
   }
 
   use 'ms-jpq/coq_nvim'
