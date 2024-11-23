3f75503c2601448e07b4fb121cb083c2dd3bf689
John Distasio <john@idiotvilla.net>
2024-11-23 13:22:54 -0500

    nvim: disable autocomplete in markdown files

--- home/config/nvim/lua/config/keymaps.lua
+++ home/config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local wk = require("which-key")
-
--- Register custom keymaps for temporary JSON and YAML file "scratchpads".
--- These will auto-format the content pasted into them and allow closing with plain "q".
-wk.add({
-    { "<leader>j", group = "scratchpad" },
-    { "<leader>jj", ":set ft=json <bar> %!jq<CR> <bar> :setlocal nobuflisted buftype=nofile bufhidden=wipe noswapfile<CR>", desc = "JSON" },
-    { "<leader>jy", ":set ft=yaml <bar> :setlocal nobuflisted buftype=nofile bufhidden=wipe noswapfile<CR>", desc = "YAML" },
+require("which-key").add({
+  -- Register custom keymaps for temporary JSON and YAML file "scratchpads".
+  -- These will auto-format the content pasted into them and allow closing with plain "q".
+  { "<leader>j", group = "scratchpad" },
+  {
+    "<leader>jj",
+    ":set ft=json <bar> %!jq<CR> <bar> :setlocal nobuflisted buftype=nofile bufhidden=wipe noswapfile<CR>",
+    desc = "JSON",
+  },
+  {
+    "<leader>jy",
+    ":set ft=yaml <bar> :setlocal nobuflisted buftype=nofile bufhidden=wipe noswapfile<CR>",
+    desc = "YAML",
+  },
 })
